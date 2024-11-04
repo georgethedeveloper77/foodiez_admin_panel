6b818ee79c808b492a7866b9d36a4a9195f6a7f6
georgethedeveloper <50877288+georgethedeveloper77@users.noreply.github.com>
2024-11-04 12:22:06 +0300

    georgethedeveloper.com

--- resources/views/layouts/footers/guest.blade.php
+++ resources/views/layouts/footers/guest.blade.php
@@ -1,11 +1,11 @@
 <footer class="footer">
-  <div class="container-fluid">
-    <div class="copyright float-right">
-      &copy;
-      <script>
-        document.write(new Date().getFullYear())
-      </script>, made with <i class="material-icons">favorite</i> by
-      <a href="" target="_blank"></a>Roy Hayek<a href="" target="_blank"></a>.
+    <div class="container-fluid">
+        <div class="copyright float-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>, made with <i class="material-icons">favorite</i> by
+            <a href="" target="_blank"></a>Mindberzerk<a href="georgethedeveloper.com" target="_blank"></a>.
+        </div>
     </div>
-  </div>
 </footer>
